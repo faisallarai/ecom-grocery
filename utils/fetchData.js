3b505285e378c880be29e3a52640cbc62a1284ee
Issaka Faisal <faisal@mpharma.com>
2021-09-27 11:31:36 +0000

    correcting bug

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -5,7 +5,7 @@ export const getData = async (url, token) => {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: token || '',
+      Authorization: token,
     },
   });
 
